571532da080b54b07a7a06f34e9e7a185302228e
Sergey Bevzenko <sbevzenko@ozon.ru>
2019-06-23 01:02:56 +0300

    Create a Login Page with aws-amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-west-1",
+        BUCKET: "notes-oke11o-v1"
+    },
+    apiGateway: {
+        REGION: "eu-west-1",
+        URL: "https://dddxicxz19.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "eu-west-1",
+        USER_POOL_ID: "eu-west-1_ZW6IeOYMm",
+        APP_CLIENT_ID: "3l5k0jmac0vsjs55it5mtrs3n2",
+        IDENTITY_POOL_ID: "eu-west-1:7fc7eb57-b72f-412e-a91b-9763becb8e7f"
+    }
+};
